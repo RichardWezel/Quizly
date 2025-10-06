36e89c43dd8a6ab4967d7eca8483bd600d1ff673
Richard Wezel <richard.wezel@posteo.de>
2025-10-06 08:43:12 +0200

    feat: created new app quiz_app with first content

--- core/settings.py
+++ core/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'auth_app',
     'rest_framework_simplejwt',
-    'rest_framework_simplejwt.token_blacklist'
+    'rest_framework_simplejwt.token_blacklist',
+    'quiz_app',
 ]
 
 MIDDLEWARE = [
---
+++ quiz_app/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from quiz_app.api.views import CreateQuizView
+
+urlpatterns = [
+    path('createQuiz/', CreateQuizView.as_view(), name='create_quiz'),
+   
+]
