fdceb31fd2c24110bf259194522cab04912b13b7
Richard Wezel <richard.wezel@posteo.de>
2025-10-06 08:51:43 +0200

    feat: created model

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Quiz(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    video_url = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    questions = models.ManyToManyField('Question', related_name='quizzes', blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, related_name='quiz_questions', on_delete=models.CASCADE)
+    question_title = models.CharField(max_length=255)
+    question_options = models.JSONField()  
+    answer = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.quiz.title} - {self.question_title}"
