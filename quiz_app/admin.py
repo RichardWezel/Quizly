db80e33ee134baeddf19a69c7ee120230c0501c9
Richard Wezel <richard.wezel@posteo.de>
2025-10-12 15:45:05 +0200

    feat: admin view for Quiz and Questions

--- quiz_app/admin.py
+++ quiz_app/admin.py
@@ -3,6 +3,14 @@ from .models import Quiz, Question
 
 @admin.register(Quiz)
 class QuizAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'description','video_url','questions', 'created_at', 'updated_at')
-    search_fields = ('title',)
+    list_display = ('id', 'title', 'description', 'video_url', 'created_at', 'updated_at')
+    search_fields = ('title', 'description')
     list_filter = ('created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question_title', 'question_options', 'answer')
+    search_fields = ('question_title',)
+    list_filter = ('id', 'question_title')
+    readonly_fields = ('id',)
