54510443d83aca74393bf0c7cb9d10a33d30aa39
Richard Wezel <richard.wezel@posteo.de>
2025-10-19 20:41:07 +0200

    refactor: admin view for owner

--- quiz_app/admin.py
+++ quiz_app/admin.py
@@ -4,9 +4,23 @@ from .models import Quiz, Question
 @admin.register(Quiz)
 class QuizAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'description', 'video_url', 'owner', 'created_at', 'updated_at')
-    search_fields = ('title', 'description')
-    list_filter = ('created_at', 'updated_at')
+    search_fields = ('title', 'description', 'owner__username', 'owner__email')
+    list_filter = ('created_at', 'updated_at', 'owner')
     readonly_fields = ('created_at', 'updated_at')
+
+    fields = (
+        "title",
+        "description",
+        "video_url",
+        "owner",         
+        "created_at",
+        "updated_at",
+    )
+
+    def save_model(self, request, obj, form, change):
+        if not change and not obj.owner_id:
+            obj.owner = request.user
+        super().save_model(request, obj, form, change)
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
